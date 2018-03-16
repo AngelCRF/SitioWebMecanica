f61c09498b833d9698b5a36b1e9574019456bf6d
Kevin Erick <kevin.angel@tecmor.mx>
2018-03-15 18:11:09 -0600

    Test

--- orga.php
+++ orga.php
@@ -3,157 +3,209 @@
 
 <html lang="es">
     <head>
-        <title>ITMORELIA| Ing. en Gestión Empresarial</title>
+        <title>ITM | Departamento Mecánica</title>
         <meta charset="utf-8"/>
-       
         <meta name="keywords" content="pagina de IGE"/>
-        
-        <meta name="author" content="Neri Gonzalez Lopez"/>
+        <meta name="author" content="Kevin Erick Angel Medrano"/>
         <link rel="icon" type="image/ico" href="Imagenes/icotec.ico"/>
-      
-         <link rel="stylesheet" type="text/css" href="index.css" />
+        <link rel="stylesheet" type="" href="orga.css" />
+        <!-- Materialized libraries -->
+        <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+           <!--Import jQuery before materialize.js-->
+        <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+        <!-- Compiled and minified JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="index.css" />
         <style type="text/css">
-        body{
-          background: #000000;
-          background-repeat: no-repeat;
-          background-position: center center;
-          background-attachment: fixed;
+            body{
+                background: #dddddd;
+                background-repeat: no-repeat;
+                background-position: center center;
+                background-attachment: fixed;
            }
         </style>
-       </head>
+    </head>
     <body >
-    <div id="header" style = "display: table; width: 500px;">
-    <div style = "float: left; width: 104px;">
-        <div style = "float: left; height: 100px; width: 100px;">
-        <ul class="nav">
-            <li><img src="Imagenes/logoch.png"></li>
+        <script type="text/javascript">
+            $(document).ready(function(){
+                $(".dropdown-button").dropdown();
+                $('.materialboxed').materialbox();
+                $(".button-collapse").sideNav();
+                $('.carousel.carousel-slider').carousel({fullWidth: true});     
+                function autoplay() {
+                    $('.carousel').carousel('next');
+                    setTimeout(autoplay, 4500);
+                }
+                jQuery(document).ready(function() {
+                    setTimeout(function() {
+                        autoplay()
+                    }, 4500);
+                });
+            });            
+        </script>
+        <script type="text/javascript" src="js/materialize.min.js"></script>
+        <!--contenedores para menus desplegables-->
+        <ul id="reticulas" class="dropdown-content">
+            <li><a href="reticula1.php">Ingeniería Mecánica</a></li>
+            <li><a href="reticula2.php">Ingeniería Mecatrónica</a></li>
         </ul>
-        </div>
-        
-    </div>
-    
-    <div  style = "float: left; height: 100px; width: 100px;">
-    	
-    	
-    	<ul class="titulo_carrera">
-			<li>Ingenieria en Gestión Empresarial</li>    	
-    	</ul>
-    	    	
-    </div>
-    
-    <div style = "height: 50px; width: 1350px;">
-    
-    <br><br>
-    	<ul class="nav">            
-            <li><a href="index.php"><font color="white">INICIO</font></a></li>
-            <li><a href="orga.php#"><font color="white">RETICULA</font></a>
-            	<ul>
-                    <li><a href="reticula1.php"><font color="white">Especialidad Calidad</font></a></li>
-                    <li><a href="reticula2.php"><font color="white">Especialidad Finanzas</font></a></li>
+        <ul id="perfiles" class="dropdown-content">
+            <li><a href="aspirante.php">Perfil de Aspirante</a></li>
+            <li><a href="egresado.php">Perfil de Egresado</a></li>
+        </ul>
+        <ul id="reprobacion" class="dropdown-content">
+            <li><a href="PDF/Indice_Rep/ENE-JUN 2015.pdf" target="blank">ENE-JUN 2015</a></li>
+            <li><a href="PDF/Indice_Rep/AGO-DIC 2015.pdf" target="blank">AGO-DIC 2015</a></li>
+            <li><a href="PDF/Indice_Rep/ENE-JUN 2016.pdf" target="blank">ENE-JUN 2016</a></li>
+            <li><a href="PDF/Indice_Rep/AGO-DIC 2016.pdf" target="blank">AGO-DIC 2016</a></li>
+        </ul>
+        <ul id="reticulas-movil" class="dropdown-content">
+            <li><a href="reticula1.php">Ingeniería Mecánica</a></li>
+             <li><a href="reticula2.php">Ingeniería Mecatrónica</a></li>
+        </ul>
+        <ul id="perfiles-movil" class="dropdown-content">
+            <li><a href="aspirante.php">Perfil de Aspirante</a></li>
+            <li><a href="egresado.php">Perfil de Egresado</a></li>
+        </ul>
+        <ul id="reprobacion-movil" class="dropdown-content">
+            <li><a href="PDF/Indice_Rep/ENE-JUN 2015.pdf" target="blank">ENE-JUN 2015</a></li>
+            <li><a href="PDF/Indice_Rep/AGO-DIC 2015.pdf" target="blank">AGO-DIC 2015</a></li>
+            <li><a href="PDF/Indice_Rep/ENE-JUN 2016.pdf" target="blank">ENE-JUN 2016</a></li>
+            <li><a href="PDF/Indice_Rep/AGO-DIC 2016.pdf" target="blank">AGO-DIC 2016</a></li>
+        </ul>   
+        <ul id="slide-out" class="side-nav">
+            <li><a href="index.php"><i class="material-icons">home</i></a></li>
+            <li><a class="dropdown-button" href="#!" data-activates="reticulas-movil">RETICULAS<i class="material-icons right" >arrow_drop_down</i></a></li>
+            <li><a href="#" class="dropdown-button" data-activates="perfiles-movil">PERFILES<i class="material-icons right" >arrow_drop_down</i></a></li>
+            <li><a href="mision.php">QUIENES SOMOS</a></li>
+            <li><a href="orga.php">ORGANIGRAMA</a></li>
+            <li><a href="material_apoyo.php">MATERIAL DE APOYO</a></li>
+            <li><a href="#" class="dropdown-button" data-activates="reprobacion-movil">INDICE DE REPROBACIÓN<i class="material-icons right" >arrow_drop_down</i></a></li>
+        </ul>
+        <!--encabezado y menus-->
+        <nav>
+            <div class="nav-wrapper grey darken-1">
+                <a href="#" data-activates="slide-out" class="button-collapse hide-on-large-only left"><i class="material-icons">menu</i></a>
+                <a href="#" class="brand-logo center"><h5>Metal Mecánica</h5></a>
+            </div>
+        </nav>
+        <nav class="hide-on-med-and-down">
+            <div class="nav-wrapper grey darken-1 center-align">
+                <ul class="center hide-on-med-and-down">
+                    <li><a href="index.php"><i class="material-icons">home</i></a></li>
+                    <li><a class="dropdown-button" href="#!" data-activates="reticulas">RETICULAS<i class="material-icons right" >arrow_drop_down</i></a></li>
+                    <li><a href="#" class="dropdown-button" data-activates="perfiles">PERFILES<i class="material-icons right" >arrow_drop_down</i></a></li>
+                    <li><a href="mision.php">QUIENES SOMOS</a></li>
+                    <li><a href="orga.php">ORGANIGRAMA</a></li>
+                    <li><a href="material_apoyo.php">MATERIAL DE APOYO</a></li>
+                    <li><a href="#" class="dropdown-button" data-activates="reprobacion">INDICE DE REPROBACIÓN<i class="material-icons right" >arrow_drop_down</i></a></li>
                 </ul>
-            </li>    
-            <li><a href="orga.php#"><font color="white">PERFILES</font></a>
-                <ul>
-                    <li><a href="aspirante.php"><font color="white">Perfil de Aspirante</font></a></li>
-                    <li><a href="egresado.php"><font color="white">Perfil de Egresado</font></a></li>
-                </ul>
-            </li>
-            <li><a href="mision.php"><font color="white">QUIENES SOMOS</font></a></li>
-            <li><a href="orga.php"><font color="white">ORGANIGRAMA</font></a></li>
-            <li><a href="material_apoyo.php"><font color="white">MATERIAL DE APOYO</font></a></li>
-          <li><a href="orga.php#"><font color="white">INDICE DE REPROBACIÓN</font></a>
-                <ul>
-                    <li><a href="PDF/Indice_Rep/ENE-JUN&#32;2015.pdf" target="blank"><font color="white">ENE-JUN 2015</font></a></li>
-                    <li><a href="PDF/Indice_Rep/AGO-DIC&#32;2015.pdf" target="blank"><font color="white">AGO-DIC 2015</font></a></li>
-                    <li><a href="PDF/Indice_Rep/ENE-JUN&#32;2016.pdf" target="blank"><font color="white">ENE-JUN 2016</font></a></li>
-                    <li><a href="PDF/Indice_Rep/AGO-DIC&#32;2016.pdf" target="blank"><font color="white">AGO-DIC 2016</font></a></li>
-              </ul>       
+            </div>
+        </nav>
+        <div class="Cprincipal_index card-panel grey lighten-4">
+            <div class="cajaS"><img src="Imagenes/logoSEP.png"></div>
+            <div class="cajaT"><img src="Imagenes/logoTEC.png"></div>
+            <div id="wrapper">
+    <div id="container">
+        <ol class="organizational-chart">
+            <li>
+                <div>
+                    <h3>Jefe de Departamento</h3>
+                </div>
+                <ol>
+                    <li>
+                        <div>
+                            <h4>Presidente de Academia</h4>
+                        </div>
+                        <ol>
+                            <li>
+                                <div>
+                                    <h5>Secretario de Academia</h5>
+                                </div>
+                            </li>
+                            <li>
+                                <div>
+                                    <h5>Jefe de Docencia</h5>
+                                </div>
+                            </li>
+                            <li>
+                                <div>
+                                    <h5>Jefe de Vinculación</h5>
+                                </div>
+                            </li>
+                            <li>
+                                <div>
+                                    <h5>Jefe de Investigación</h5>
+                                </div>
+                            </li>
+                        </ol>
                     </li>
-                
-        </ul>
-    
+                    <li>
+                        <div>
+                            <h4>Profesores</h4>
+                        </div>
+                        <ol>
+                            <li>
+                                <div>
+                                    <h5>Profesor 1</h5>
+                                </div>
+                            </li>
+                            <li>
+                                <div>
+                                    <h5>Profesor 2</h5>
+                                </div>
+                            </li>
+                        </ol>
+                    </li>
+                </ol>
+            </li>
+        </ol>
+
     </div>
-    
 </div>
-<br><br><br>
-
-
-<div class="Cprincipal_organ">
-  <div class="cajaS"><img src="Imagenes/logoSEP.png"></div>
-     <div class="cajaT"><img src="Imagenes/logoTEC.png"><br></div>
-     <br><br><br><br>
-
-    
-
-	 <div id="Organigrama">
-          <img src="Imagenes/Organigrama5.png">
-          <a id="Jefatura" target="_blank" href="PDF/CV/JoseAlvaroMerlos.pdf">cv</a>
-          <a id="Presidencia_Academica" target="_blank" href="PDF/CV/JesusMancera.pdf">cv</a>
-          <a id="Secretario_Academico" target="_blank" href="PDF/CV/RobertoYoung.pdf">cv</a>
-          <a id="Docencia" target="_blank" href="PDF/CV/RafaelMadrigal.pdf">cv</a>
-          <a id="Investigacion" target="_blank" href="PDF/CV/RaulRivera.pdf">cv</a>
-          <a id="Vinculacion" target="_blank" href="PDF/CV/AlejandroRios.pdf">cv</a>
-     <!-- <a id="Vinculacion" target="_blank" href="PDF/CV/AlejandroRios.pdf">Curriculum Vitae</a>  -->                
-      </div> 
-      <div id="Organigrama2">
-      	  <a id="Docente_1" target="_blank" href="PDF/CV/TereVillasenor.pdf">CV</a>    
-          <a id="Docente_2" target="_blank" href="PDF/CV/CesarLuna.pdf">CV</a>
-          <a id="Docente_3" target="_blank" href="PDF/CV/CV.pdf">CV</a>
-          <a id="Docente_4" target="_blank" href="PDF/CV/CV.pdf">CV</a>
-          <a id="Docente_5" target="_blank" href="PDF/CV/AgustinEliseo.pdf">CV</a>
-          <a id="Docente_6" target="_blank" href="PDF/CV/MiriamMadrigal.pdf">CV</a>
-          <a id="Docente_7" target="_blank" href="PDF/CV/AlbertoCeron.pdf">CV</a>
-          <a id="Docente_8" target="_blank" href="PDF/CV/Jose&#32;Antonio&#32;Mejia.pdf">CV</a>
-          <a id="Docente_9" target="_blank" href="PDF/CV/PatriciaDiaz.pdf">CV</a>
-          <a id="Docente_21" target="_blank" href="PDF/CV/CV.pdf">CV</a>
-          <a id="Docente_11" target="_blank" href="PDF/CV/ArturoGandara.pdf">CV</a>       
-          <a id="Docente_12" target="_blank" href="PDF/CV/MigualASalinas.pdf">CV</a>
-          <a id="Docente_13" target="_blank" href="PDF/CV/Gabriela&#32;Garcia&#32;Diaz.pdf">CV</a>
-          <a id="Docente_14" target="_blank" href="PDF/CV/JoseluisSoria.pdf">CV</a>
-          <a id="Docente_15" target="_blank" href="PDF/CV/GuillermoGutierrezHernandez.pdf">CV</a>
-          <a id="Docente_16" target="_blank" href="PDF/CV/DavidSoto.pdf">CV</a>
-          <a id="Docente_17" target="_blank" href="PDF/CV/SamaraHerrera.pdf">CV</a>
-          <a id="Docente_18" target="_blank" href="PDF/CV/PaolaVillasenor.pdf">CV</a>
-          <a id="Docente_19" target="_blank" href="PDF/CV/RaymundoHererra.pdf">CV</a>
-          <a id="Docente_22" target="_blank" href="http://sagitario.itmorelia.edu.mx/ige/PDF/CV/Rosario.pdf">CV</a>
-        <!-- <a id="Docente_23" target="_blank" href="PDF/CV/Livia Sanchez Gonzalez.pdf">CV</a>  -->  
-                           
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br
+            ><br><br><br>
         </div>
-
-<img alt="" src="orga.php" >
-
-</div>
-
-
-<div  class="letra_pie">
-<div class="Cpie_index2">
-<div class="CSocial">
-<div class="titulos_pie"><center>Redes Sociales</center><br></div>
-<div class="CSocial2">
-<a id="link" href="https://www.facebook.com/IGE-ITMorelia-Oficial-137692846975721/" target="_blank"> <img src="Imagenes/face.jpg">Facebook<br></a>
-<a  id="link" href="https://twitter.com/itmoficial" target="_blank"> <img src="Imagenes/tiwter.jpg">Twitter<br></a>
-<a  id="link" href="https://www.youtube.com/user/ITMoreliaOficial/" target="_blank"> <img src="Imagenes/youtube.jpg">Youtube<br> </a>
-<a  id="link" href="https://www.linkedin.com/edu/instituto-tecnol%C3%B3gico-de-morelia-173011" target="_blank"><img src="Imagenes/link.jpg">Linkedin<br></a>
-</div>
-</div>
-<div class="CVinculo">
-<div class="titulos_pie">Vínculos Académicos</center><br><br></div>
-<a  id="link" href="http://www.tecnm.mx/" target="_blank">Tecnológico Nacional De México<br><br></a>
-<a   id="link" href="http://www.ets.org/es/toefl"  target="_blank">Toefl<br><br></a>
-<a   id="link" href="http://www.ceneval.edu.mx/" target="_blank">Ceneval<BR><br></a>
-<a    id="link" href="http://www.anfei.mx/" target="_blank">Anfei<BR><br></a>
-<a     id="link" href="http://www.conacyt.mx/" target="_blank">Consejo Nacional De Ciencia Y Tecnología<br><br></a>
-</div>
-<div class="CContactar">
-<div class="titulos_pie">Contactanos</div><br><br>
-<a id="link2">Teléfonos:</a><br>
-4433121570 Ext.248<br><br>
-<a id="link2">Email:</a><br><a id="link" >ige@itmorelia.edu.mx</a><br><br>
-<a id="link2">Dirección:</a><br>
-Avenida Tecnológico No. 1500. Lomas de Santiaguito, Edificio AA. C.P. 58120 Morelia, Michoacán, México.<br>
-</div> <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-Derechos Reservados. © 2017 Instituto Tecnológico de Morelia.
-</div>
-
+        <footer class="page-footer grey darken-1">
+            <div class="container">
+                <div class="row">
+                    <div class="col l4 s12">
+                        <h5 class="white-text">Redes Sociales</h5>
+                        <ul>
+                            <li><a class="grey-text text-lighten-3" href="https://www.facebook.com/IGE-ITMorelia-Oficial-137692846975721/" target="_blank"><img src="Imagenes/face.jpg">Facebook</a></li>
+                            <li><a class="grey-text text-lighten-3" href="https://twitter.com/itmoficial" target="_blank"> <img src="Imagenes/tiwter.jpg">Twitter</a></li>
+                            <li><a class="grey-text text-lighten-3" href="https://www.youtube.com/user/ITMoreliaOficial/" target="_blank"> <img src="Imagenes/youtube.jpg">Youtube</a></li>
+                            <li><a class="grey-text text-lighten-3" href="https://www.linkedin.com/edu/instituto-tecnol%C3%B3gico-de-morelia-173011" target="_blank"><img src="Imagenes/link.jpg">Linkedin</a></li>
+                        </ul>
+                    </div>
+                    <div class="col l4 s12">
+                        <h5 class="white-text">Vínculos Académicos</h5>
+                        <ul>
+                            <li><a class="grey-text text-lighten-3" href="http://www.tecnm.mx/" target="_blank">Tecnológico Nacional De México</a></li>
+                            <li><a class="grey-text text-lighten-3" href="http://www.ets.org/es/toefl"  target="_blank">Toefl</a></li>
+                            <li><a class="grey-text text-lighten-3" href="http://www.ceneval.edu.mx/" target="_blank">Ceneval</a></li>
+                            <li><a class="grey-text text-lighten-3" href="http://www.anfei.mx/" target="_blank">Anfei</a></li>
+                            <li><a class="grey-text text-lighten-3" href="http://www.conacyt.mx/" target="_blank">Consejo Nacional De Ciencia Y Tecnología</a></li>
+                        </ul>
+                    </div>
+                    <div class="col l4 s12">
+                        <h5 class="white-text">Contáctanos</h5>
+                        <ul>
+                            Teléfonos: 4433121570 Ext.248<br />
+                            Email: metal_Mecánica@itmorelia.edu.mx<br />
+                            Dirección: Avenida Tecnológico No. 1500. Lomas de Santiaguito, Edificio AA. C.P. 58120 Morelia, Michoacán, México.<br />
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-copyright">
+                <div class="container">
+                    Derechos Reservados. © 2018 Instituto Tecnológico de Morelia.
+                </div>
+            </div>
+        </footer>
     </body>
-    </html>
+</html>
