bbea347c7674bf9cd46b8f7eaeb0c57afc8bd689
Luis Eduardo Loaiza Diosdado <luis.loaiza@tecmor.mx>
2018-04-21 19:04:40 -0500

    rev conexion mysql

--- ConexionBD.php
+++ ConexionBD.php
@@ -1,10 +1,29 @@
 <?php
-	$conexion = mysqli_connect("ip","usuario","contraseña","base de datos");
-	if(!$conexion){
-		echo "No se pudo conectar a base de datos";
-		exit;
-	}else{
-		echo "Mensajes solo para hacer pruebas, si ves esto, la conexión es correcta";
-		$mysqli -> mysqli_close();
+	class conexionBD
+	{				
+		private $servidor = "localhost";
+		private $usuario = "root";
+		private $contrasena="";
+		private $bd = "cifrado";
+		private $conexion;
+		
+		function conexionBD($servidor,$bd,$usuario,$contrasena){
+			$this->servidor = $servidor;
+			$this->bd = $bd;
+			$this->usuario = $usuario;
+			$this->contrasena = $contrasena;
+			$this->conectar();
+
+		}
+
+		private function conectar(){
+			$this->conexion = mysqli_connect($this->$servidor,$this->usuario,$this->contrasena) or die (mysqli_error());
+		}
+
+		private function seleccionarbd(){
+			mysqli_select_db($this->bd) or die(mysqli_error());
+		}
+
+		
 	}
 ?>
