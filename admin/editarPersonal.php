b26a6ba7d98dd1624291589ad66e4262d87ce39f
cvc23 <cvc2310@hotmail.com>
2018-05-02 11:21:50 -0500

    identacion de código...

--- admin/editarPersonal.php
+++ admin/editarPersonal.php
@@ -9,53 +9,53 @@ if(isset($_POST) and $_SERVER["REQUEST_METHOD"]=="POST"){
     extract($_POST);
     
     if( (isset($_FILES['archivoe']) && $_FILES['archivoe']['error'] != 4) && (isset($_FILES['archivoFotoe']) && $_FILES['archivoFotoe']['error'] != 4)  ) { 
-            //echo "llegue al primer if donde ambos archivos existen\n"; 
-            //se debe cambiar toda la información del profesor
-            //asignar las rutas para guardar los archivos de foto(png, jpg, etc) y CV (pdf)
-            $targetfolder = "CV/";
-            $targetfoto = "foto/";
-            $ruta= "";
-            $targetfolder = $targetfolder . basename( $_FILES['archivoe']['name']);
-            $targetfoto = $targetfoto . basename( $_FILES['archivoFotoe']['name']);
-           if(move_uploaded_file($_FILES['archivoe']['tmp_name'], $targetfolder))
-            {
-                //echo "The file ". basename( $_FILES['archivo']['name']). " is uploaded";
+        //echo "llegue al primer if donde ambos archivos existen\n"; 
+        //se debe cambiar toda la información del profesor
+        //asignar las rutas para guardar los archivos de foto(png, jpg, etc) y CV (pdf)
+        $targetfolder = "CV/";
+        $targetfoto = "foto/";
+        $ruta= "";
+        $targetfolder = $targetfolder . basename( $_FILES['archivoe']['name']);
+        $targetfoto = $targetfoto . basename( $_FILES['archivoFotoe']['name']);
+       if(move_uploaded_file($_FILES['archivoe']['tmp_name'], $targetfolder))
+        {
+            //echo "The file ". basename( $_FILES['archivo']['name']). " is uploaded";
+        }
+        else {
+            //echo "Problem uploading file";
+        }
+        if(move_uploaded_file($_FILES['archivoFotoe']['tmp_name'], $targetfoto))
+        {
+            //echo "The file ". basename( $_FILES['archivo']['name']). " is uploaded";
+        }
+        else {
+           //echo "Problem uploading file";
+        }
+        $archivo = $targetfolder;
+        $foto = $targetfoto;
+        //Eliminar el archivo viejo de CV y foto
+        $conexion = new DB();
+        $oldCV = $conexion->ejecutar("SELECT CV FROM Profesor WHERE nombre='".$nombreprofesorEditar."'");
+        foreach($oldCV as $fila){
+            $ruta = $fila[0];
+        }
+        unlink($ruta);
+        $ruta= "";
+        $oldPhoto = $conexion->ejecutar("SELECT foto FROM Profesor WHERE nombre='".$nombreprofesorEditar."'");
+        foreach($oldPhoto as $fila){
+        $ruta = $fila[0];
             }
-            else {
-                //echo "Problem uploading file";
-                }
-            if(move_uploaded_file($_FILES['archivoFotoe']['tmp_name'], $targetfoto))
-            {
-                //echo "The file ". basename( $_FILES['archivo']['name']). " is uploaded";
-            }
-            else {
-                //echo "Problem uploading file";
-            }
-            $archivo = $targetfolder;
-            $foto = $targetfoto;
-            //Eliminar el archivo viejo de CV y foto
-            $conexion = new DB();
-            $oldCV = $conexion->ejecutar("SELECT CV FROM Profesor WHERE nombre='".$nombreprofesorEditar."'");
-            foreach($oldCV as $fila){
-                $ruta = $fila[0];
-                }
-            unlink($ruta);
-            $ruta= "";
-            $oldPhoto = $conexion->ejecutar("SELECT foto FROM Profesor WHERE nombre='".$nombreprofesorEditar."'");
-            foreach($oldPhoto as $fila){
-                $ruta = $fila[0];
-                }
-            unlink($ruta);
-            $ruta= "";
-            $resultado = $conexion->editarProfesor($nombreprofesorEditar, $puestoeditar, $carreraeditar, $archivo, $foto);
-            if($resultado>0){
-                $_SESSION['result'] = 'editado';
-                header('Location: prueba_admin.php');
-            }
-            else{
-                $_SESSION['result'] = 'error';
-                header('Location: prueba_admin.php'); 
-            }
+        unlink($ruta);
+        $ruta= "";
+        $resultado = $conexion->editarProfesor($nombreprofesorEditar, $puestoeditar, $carreraeditar, $archivo, $foto);
+        if($resultado>0){
+            $_SESSION['result'] = 'editado';
+            header('Location: prueba_admin.php');
+        }
+        else{
+            $_SESSION['result'] = 'error';
+            header('Location: prueba_admin.php'); 
+        }
     }
     else{
                 
@@ -78,78 +78,74 @@ if(isset($_POST) and $_SERVER["REQUEST_METHOD"]=="POST"){
             $ruta= "";
             $oldCV = $conexion->ejecutar("SELECT CV FROM Profesor WHERE nombre='".$nombreprofesorEditar."'");
             foreach($oldCV as $fila){
-                        $ruta = $fila[0];
-                        //echo $fila[0];
-                    }
-                    //echo $ruta;
-                    //echo "unlinked";
-                    unlink($ruta);
-                    $ruta= "";
-                    $resultado = $conexion->editarProfesorCV($nombreprofesorEditar, $puestoeditar, $carreraeditar, $archivo);
-                    if($resultado>0){
-                        header('Location: prueba_admin.php');
-                        $_SESSION['result'] = 'editado';
-                    }
-                    else{
-                        header('Location: prueba_admin.php');
-                        $_SESSION['result'] = 'error';
-                    }
-                    $_SESSION['result'] = 'editado';
+                $ruta = $fila[0];
+            }
+            unlink($ruta);
+            $ruta= "";
+            $resultado = $conexion->editarProfesorCV($nombreprofesorEditar, $puestoeditar, $carreraeditar, $archivo);
+            if($resultado>0){
+                header('Location: prueba_admin.php');
+                $_SESSION['result'] = 'editado';
+            }
+            else{
+                header('Location: prueba_admin.php');
+                $_SESSION['result'] = 'error';
+            }
+            $_SESSION['result'] = 'editado';
+        }
+        else {
+            if( (isset($_FILES['archivoe']) && $_FILES['archivoe']['error'] == 4) && (isset($_FILES['archivoFotoe']) && $_FILES['archivoFotoe']['error'] != 4)  ) { 
+                //echo "llegue al tercer if donde solo la foto existe";
+                //asignar las rutas para guardar los archivos de foto(png, jpg, etc)
+                $targetfoto = "foto/";
+                $targetfoto = $targetfoto . basename( $_FILES['archivoFotoe']['name']) ;
+                if(move_uploaded_file($_FILES['archivoFotoe']['tmp_name'], $targetfoto))
+                {
+                    //echo "The file ". basename( $_FILES['archivo']['name']). " is uploaded";
                 }
                 else {
-                    if( (isset($_FILES['archivoe']) && $_FILES['archivoe']['error'] == 4) && (isset($_FILES['archivoFotoe']) && $_FILES['archivoFotoe']['error'] != 4)  ) { 
-                        //echo "llegue al tercer if donde solo la foto existe";
-                    //asignar las rutas para guardar los archivos de foto(png, jpg, etc)
-                    $targetfoto = "foto/";
-                    $targetfoto = $targetfoto . basename( $_FILES['archivoFotoe']['name']) ;
-                    if(move_uploaded_file($_FILES['archivoFotoe']['tmp_name'], $targetfoto))
-                    {
-                        //echo "The file ". basename( $_FILES['archivo']['name']). " is uploaded";
-                    }
-                    else {
-                        //echo "Problem uploading file";
-                    }
-                    $foto = $targetfoto;
-                    //Eliminar el archivo viejo de CV y foto
-                    $conexion = new DB();
-                    $ruta= "";
-                    $oldPhoto = $conexion->ejecutar("SELECT foto FROM Profesor WHERE nombre='".$nombreprofesorEditar."'");
-                    foreach($oldPhoto as $fila){
-                        $ruta = $fila[0];
-                        //echo $fila[0];
-                    }
-                    unlink($ruta);
-                    //se editará el puesto, carrera y foto
-                    $resultado = $conexion->editarProfesorFoto($nombreprofesorEditar, $puestoeditar, $carreraeditar, $foto);
-                    if($resultado>0){
-                        header('Location: prueba_admin.php');
-                        $_SESSION['result'] = 'editado';
-                    }
-                    else{
-                        header('Location: prueba_admin.php');
-                        $_SESSION['result'] = 'error';
-                    }
-
-                        $_SESSION['result'] = 'editado';
-                    }
-                    else{
-                        //el usuario no ingresó ningun archivo por lo que solo se modificará el puesto y carrera
-                        //echo "llegue al ultimo if donde no hay archivos";
-                        $conexion = new DB();
-                        $resultado = $conexion->editarProfesorPuesto($nombreprofesorEditar, $puestoeditar, $carreraeditar);
-                    
-                        $_SESSION['result'] = 'editado';
-                        if($resultado>0){
-                            //header('Location: prueba_admin.php');
-                        }
-                        else{
-                            header('Location: prueba_admin.php');
-                            $_SESSION['result'] = 'error';
-                        }
-
-                    }
+                    //echo "Problem uploading file";
                 }
-            }   
+                $foto = $targetfoto;
+                //Eliminar el archivo viejo de CV y foto
+                $conexion = new DB();
+                $ruta= "";
+                $oldPhoto = $conexion->ejecutar("SELECT foto FROM Profesor WHERE nombre='".$nombreprofesorEditar."'");
+                foreach($oldPhoto as $fila){
+                    $ruta = $fila[0];
+                    //echo $fila[0];
+                }
+                unlink($ruta);
+                //se editará el puesto, carrera y foto
+                $resultado = $conexion->editarProfesorFoto($nombreprofesorEditar, $puestoeditar, $carreraeditar, $foto);
+                if($resultado>0){
+                    header('Location: prueba_admin.php');
+                    $_SESSION['result'] = 'editado';
+                }
+                else{
+                    header('Location: prueba_admin.php');
+                    $_SESSION['result'] = 'error';
+                }
+
+            }
+            else{
+                //el usuario no ingresó ningun archivo por lo que solo se modificará el puesto y carrera
+                //echo "llegue al ultimo if donde no hay archivos";
+                $conexion = new DB();
+                $resultado = $conexion->editarProfesorPuesto($nombreprofesorEditar, $puestoeditar, $carreraeditar);
+                    
+                
+                if($resultado>0){
+                    header('Location: prueba_admin.php');
+                    $_SESSION['result'] = 'editado';
+                }
+                else{
+                    header('Location: prueba_admin.php');
+                    $_SESSION['result'] = 'error';
+                }
+            }
+        }
+    }   
 }
 else{
     echo "se genero un error";
