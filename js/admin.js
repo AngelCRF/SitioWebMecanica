a2333e9402bdbaadb5c88d016bc41f0b1135375c
cvc23 <cvc2310@hotmail.com>
2018-05-02 21:09:27 -0500

    modificacion de codigo para validar errores

--- js/admin.js
+++ js/admin.js
@@ -16,7 +16,11 @@ function agregarpersonal(){
     $('select').material_select();
 }
 $(function(){
-    $('#agregarPersonalForm').submit(function() {
+
+    $('#agregarPersonalForm').on("submit", function(e){
+        e.preventDefault();
+        var f = $(this);
+        var formData = new FormData(document.getElementById("agregarPersonalForm"));
         var validated =0;
         if($('#nombreprofesor').val().length == 0 ){
             
@@ -35,7 +39,7 @@ $(function(){
         if(validated>=1){
             switch(validated){
                 case 1: alert("Favor de ingresar un nombre valido"); break;
-                case 2: alert("Favor de ingrear un archivo PDF en curriculum"); break;
+                case 2: alert("Favor de ingresar un archivo PDF en curriculum"); break;
                 case 4: alert("Favor de ingresar una imagen para el perfil del profesor"); break;
                 case 5: alert("Favor de ingresar un nombre y fotografía valiada"); break;
                 case 6: alert("Favor de ingresar una foto y archivo PDF validos"); break;
@@ -46,51 +50,87 @@ $(function(){
             return false;
         }
         else{
-            return true;
-        }
-    });
-
-    /*
-    $('#editarPersonalForm').submit(function() {
-        var validated =0;
-        if($('#nombreprofesorEditar').val() == "disable" ){
-            validated =validated+ 1;
-            $("#nombreprofesorEditar").css('background-color', '#fbc7c7');
-        }
-        if($('#archivoe').get(0).files.length === 0){
-            $("#archivo").css('background-color', '#fbc7c7');
-            validated =validated + 2;
-        }
-        if($('#archivoFotoe').get(0).files.length === 0){
-
-            $("#archivoFoto").css('background-color', '#fbc7c7');
-            validated =validated + 4;
-        }
-        if(validated>=1){
-            switch(validated){
-                case 1: alert("Favor de ingresar un nombre valido"); break;
-                case 2: alert("Favor de ingrear un archivo PDF en curriculum"); break;
-                case 4: alert("Favor de ingresar una imagen para el perfil del profesor"); break;
-                case 5: alert("Favor de ingresar un nombre y fotografía valiada"); break;
-                case 6: alert("Favor de ingresar una foto y archivo PDF validos"); break;
-                case 7: alert("Favor de llenar todos los campos"); break;
-                default: ;break;
-            }
             
-            return false;
-        }
-        else{
-            return true;
+        $.ajax({
+            url: "subirPersonal.php", // Url to which the request is send
+            type: "POST",             // Type of request to be send, called as method
+            data: formData, // Data sent to server, a set of key/value pairs (i.e. form fields and values)
+            contentType: false,       // The content type used when sending data to the server.
+            cache: false,             // To unable request pages to be cached
+            processData:false,        // To send DOMDocument or non processed data file it is set to false
+            success: function(data)   // A function to be called if request succeeds
+            {
+                if(data.status == 'success'){
+                    alert("Profesor agregado correctamente");
+                    document.getElementById("agregarPersonalForm").reset();
+                }else if(data.status == 'errorArchivo'){
+                    alert("Error al subir el CV");
+                }else if(data.status == 'errorFoto'){
+                    alert("Error al subir la foto");
+                }else if(data.status == 'errorConsulta'){
+                    alert("Error de conexión, vuela a intentarlo más tarde");
+                }
+                
+            }
+            });
+            
+    
+     
+            //return true;
         }
     });
-    */
-    $('#eliminarPersonalForm').submit(function() {
+
+    
+    $('#editarPersonalForm').on("submit", function(e){
+        e.preventDefault();
+        var formData = new FormData(document.getElementById("editarPersonalForm"));
+            $.ajax({
+                url: "editarPersonal.php", // Url to which the request is send
+                type: "POST",             // Type of request to be send, called as method
+                data: formData, // Data sent to server, a set of key/value pairs (i.e. form fields and values)
+                contentType: false,       // The content type used when sending data to the server.
+                cache: false,             // To unable request pages to be cached
+                processData:false,        // To send DOMDocument or non processed data file it is set to false
+                success: function(data)   // A function to be called if request succeeds
+                {
+                    if(data.status == 'success'){
+                        alert("Profesor editado correctamente");
+                        document.getElementById("agregarPersonalForm").reset();
+                    }else if(data.status == 'errorArchivo'){
+                        alert("Error al subir el CV");
+                    }else if(data.status == 'errorFoto'){
+                        alert("Error al subir la foto");
+                    }else if(data.status == 'errorConsulta'){
+                        alert("Error de conexión, vuela a intentarlo más tarde");
+                    }
+                }
+                });
+            //return true;
+        
+    });
+    
+    $('#eliminarPersonalForm').on("submit", function(e){
+        e.preventDefault();
+        var formData = new FormData(document.getElementById("eliminarPersonalForm"));
         if(jQuery('#eliminarp').val() == "x" ){
             alert("Favor de elegir un profesor para eliminar");
             return false;
         }
         else{
-            return true;
+            $.ajax({
+                url: "eliminarPersonal.php", // Url to which the request is send
+                type: "POST",             // Type of request to be send, called as method
+                data: formData, // Data sent to server, a set of key/value pairs (i.e. form fields and values)
+                contentType: false,       // The content type used when sending data to the server.
+                cache: false,             // To unable request pages to be cached
+                processData:false,        // To send DOMDocument or non processed data file it is set to false
+                success: function(data)   // A function to be called if request succeeds
+                {
+                    alert("Profesor eliminado correctamente");
+                    document.getElementById("eliminarPersonalForm").reset();
+                }
+                });
+            //return true;
         }
         
     });
